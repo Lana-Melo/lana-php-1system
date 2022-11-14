17d60e3430ef1170ffda66842da724fb6a8d3366
Lana Melo <lanamelo2015@gmail.com>
2022-11-14 21:35:43 +0000

    first commit

---
+++ inicial.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Lana";
+$idade = 22;
+$dataDeNascimento = date("d-m-Y");
+
+
+echo "olá, meu nome é " . $nome . ". Eu tenho " . $idade . " anos. " . "Hoje é " . $dataDeNascimento;
+?>
+
+
