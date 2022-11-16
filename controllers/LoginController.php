e96222751897eb07c9da3782b615a00204f96b33
Lana Melo <lanamelo2015@gmail.com>
2022-11-16 22:15:45 +0000

    adding a database to the project to improve its login process

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,11 +3,12 @@
 
 include "../models/LoginModel.php";
 
+
 // receiving variables from views/initial.php
 $email=$_GET['email'];
 $password=$_GET['password'];
 
-// validating email
+// validating email to not make useless contact with db
 if(empty($email)){
 	header("Location: /sistemadalana/views/inicial.php");
 }
@@ -16,7 +17,12 @@ if(empty($email)){
 $logindata = new Login;
 $response = $logindata->signin($email, $password);
 
-// printing the response
-echo $response;
+// if user exists, echo a welcome message. otherwise, echo a error message
+if ($response) {
+	echo "ola, ".$_SESSION['userexists'];
+}
+else {
+	echo "User not found";
+}
 
 ?>
--- models/LoginModel.php
+++ models/LoginModel.php
@@ -1,13 +1,27 @@
 <?php  
 class Login {
+
+	// check if user exists and return its name
 	public function signin($email, $password) {
-		if (empty($email) && empty($password)) {
+
+		// creating connection with db and adding it to the $con variable
+		$con = mysqli_connect("localhost","lana","123","dblana");
+
+		// making a query (email & password) and storing the query object into $result variable
+		$result = mysqli_query($con, "SELECT * FROM user WHERE email ='".$email."' and password='".$password."'");
+
+		// turning the query object into an array
+		$row = mysqli_fetch_row($result);
+		
+		// check if user exists 
+		if (empty($row)){
 			return false;
 		}
-		else {
-			return true;
-		}
-	}
 
+		// starting a session and creating a session variable with the username
+		session_start();
+		$_SESSION['userexists']=$row[1];
+		return true;
+	}
 }
 ?>
