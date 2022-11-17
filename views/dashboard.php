16c007ddfcff4edb2b339cb41b2cd2c30f3bb806
Lana Melo <lanamelo2015@gmail.com>
2022-11-17 22:47:09 +0000

    Adding a dashboard page to redirect user after login

---
+++ views/dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles.css">
+	<title>Dashboard</title>
+</head>
+<body>
+	<div class="pagecontainer">	
+		<div class="main_menu">
+			<button type="button">MEUS CURSOS</button>
+			<button type="button">LISTAR CURSOS</button>
+			<button type="button">PERFIL</button>
+		</div>
+		<div class="allcontent">
+			<div class="title">
+				<h1>Olá, [userame]<h1>
+			</div>
+			<div class="content">
+				<p>Hmm, parece que você ainda não está inscrito em nenhum curso</p>
+			</div>
+				
+		</div>
+	</div>
+</body>
+</html>
